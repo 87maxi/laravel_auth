3df4a66f858c2fd88b16c5dddc773458a2e15a5f
maxi <maxiparedes@gmail.com>
2021-10-25 15:35:02 -0300

    fix db

--- dc/php-mysqli/entrypoint.sh
+++ dc/php-mysqli/entrypoint.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -Eeo pipefail
 
 chown -R www-data:www-data /var/www/html/storage/framework;
 chown -R www-data:www-data /var/www/html/storage/logs;
+ 
+php artisan migrate:fresh
 
-php artisan migrate
-
-if [ $? != 0 ] 
-then
-  echo "<<<<ERROR>>>>"
-  exit 1
-else
-    php-fpm
-fi
+php-fpm
