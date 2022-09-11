11dd0f118ffcbe7fd7cee8a57b120a06661c10df
maxi <maxiparedes@gmail.com>
2022-09-11 13:43:39 -0300

    css, js, nginx

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -10,7 +10,7 @@
                 {{-- Google Analytics Embed --}}
                 <div id="embed-api-auth-container"></div>
             @else
-                <p style="border-radius:4px; padding:20px; background:#fff; margin:0; color:#999; text-align:center;">
+                <p style="border-radius:4px; padding:20px; background:#fff; margin:0; color:#000; text-align:center;">
                     {!! __('voyager::analytics.no_client_id') !!}
                     <a href="https://console.developers.google.com" target="_blank">https://console.developers.google.com</a>
                 </p>
