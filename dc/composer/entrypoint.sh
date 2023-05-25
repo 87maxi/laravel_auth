621f98cd58f19020da7aa33f856fd579c4d355b5
maxi <maxi@localhost.localdomain>
2023-05-25 11:03:59 -0300

    add storage file

--- dc/composer/entrypoint.sh
+++ dc/composer/entrypoint.sh
@@ -3,13 +3,14 @@
 
 cd /app;
 
-cd storage
-mkdir logs
-mkdir framework
-mkdir framework/cache && framework/cache/data
-mkdir framework/sessions
-mkdir framework/testing
-mkdir framework/views
+mkdir storage;
+cd storage;
+mkdir logs;
+mkdir framework;
+mkdir framework/cache && framework/cache/data;
+mkdir framework/sessions;
+mkdir framework/testing;
+mkdir framework/views;
 
 chgrp -R www-data ../storage
 chown -R www-data ../storage
